61d7fc70cb403d4cac1a0d477abcebcb5e835643
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-06-06 23:44:37 +0530

    uipdate

--- flake.nix
+++ flake.nix
@@ -58,7 +58,8 @@
   inputs.flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" "aarch64-linux" ]; 
     imports = [ 
-      (inputs.import-tree ./modules) 
+      (inputs.import-tree ./modules)
+      inputs.cosmic.flakeModules.default 
     ];
   };
 }
