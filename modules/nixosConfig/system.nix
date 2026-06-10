ef3763d5b61c9875e338015761badb08616ce926
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-06-10 20:00:42 +0530

    update

--- modules/nixosConfig/system.nix
+++ modules/nixosConfig/system.nix
@@ -6,18 +6,12 @@
     ];
     nix.settings = {
       experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
-      trusted-users = [ "root" "sudha" ];
     };
     programs.nix-ld = {
       enable = true;
     };
     nixpkgs.config.allowUnfree = true;
     system.stateVersion = "26.05";
-    networking = {
-      networkmanager.enable = true;
-      firewall.enable = false;
-    };
-
     time.timeZone = "Asia/Kolkata";
     i18n.defaultLocale = "en_US.UTF-8";
     console.keyMap = "us";
@@ -33,12 +27,8 @@
         wireplumber.enable = true; # The modern session manager that handles dynamic routing
       };
       openssh.enable = true;
-      avahi = {
-        enable = true;
-        nssmdns4 = true; # Allows the laptop (and all nodes) to resolve .local
-      };
+
     };
-    
     environment.systemPackages = with pkgs; [
       tree 
       util-linux 
