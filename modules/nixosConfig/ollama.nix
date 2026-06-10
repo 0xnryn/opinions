dc3a76a5390f3c7e3db59a9c6a68a8307c62c151
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-06-10 10:55:33 +0530

    update

--- modules/nixosConfig/ollama.nix
+++ modules/nixosConfig/ollama.nix
@@ -3,15 +3,16 @@
   flake.nixosModules.ollama_cuda = { pkgs, ... }:
   {
     services.ollama = {
-    enable = true;
-    
-    # Explicitly use the CUDA-compiled package instead of the acceleration flag
-    package = pkgs.ollama-cuda; 
-    
-    environmentVariables = {
-      CUDA_VISIBLE_DEVICES = "0";
-      GGML_VK_VISIBLE_DEVICES = "0";
-    };
+        enable = true;
+        
+        # Explicitly use the CUDA-compiled package instead of the acceleration flag
+        package = pkgs.ollama-cuda; 
+        
+        environmentVariables = {
+          CUDA_VISIBLE_DEVICES = "0";
+          GGML_VK_VISIBLE_DEVICES = "0";
+        };
+      };
   };
 
   flake.nixosModules.ollama = { pkgs, ... }:
