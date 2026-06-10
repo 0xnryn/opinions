86fc53ac6b5148acb7fd2b4753cbf682c6c41e55
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-06-10 10:52:48 +0530

    update

--- modules/nixosConfig/ollama.nix
+++ modules/nixosConfig/ollama.nix
@@ -4,7 +4,15 @@
   {
     services.ollama = {
       enable = true;
-      package = pkgs.ollama-cuda;
+      acceleration = "cuda";
+      
+      # Force Ollama to ignore the integrated graphics and only use the dGPU
+      environmentVariables = {
+        CUDA_VISIBLE_DEVICES = "0";
+        
+        # If your system ever falls back to Vulkan, lock that too:
+        GGML_VK_VISIBLE_DEVICES = "0";
+      };
     };
   };
 
