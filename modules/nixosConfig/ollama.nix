785b02458aca7a2ab0c19d19e1bd7ff6983c0150
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-06-10 11:09:29 +0530

    update

--- modules/nixosConfig/ollama.nix
+++ modules/nixosConfig/ollama.nix
@@ -4,20 +4,7 @@
   {
     services.ollama = {
       enable = true;
-      
-      # Explicitly use the CUDA-compiled package instead of the acceleration flag
       package = pkgs.ollama-cuda; 
-      
-      environmentVariables = {
-        # Keep your existing CUDA lock
-        CUDA_VISIBLE_DEVICES = "0";
-        
-        # Add the fixes from the GitHub issue
-        OLLAMA_LLM_LIBRARY = "cuda";
-        OLLAMA_IGPU_ENABLE = "0";
-        OLLAMA_VULKAN = "false";
-        HSA_OVERRIDE_GFX_VERSION = "";
-      };
     };
   };
 
