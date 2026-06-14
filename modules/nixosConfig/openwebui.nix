02b9924b6d13ea19af527f33fc86e181a26c3736
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-06-14 21:27:53 +0530

    update

--- modules/nixosConfig/openwebui.nix
+++ modules/nixosConfig/openwebui.nix
@@ -4,25 +4,10 @@
     services.open-webui = {
       enable = true;
       port = 8080;
-      
       environment = {
-        OLLAMA_BASE_URL = "http://127.0.0.1:11434";
-        ENABLE_OPENAI_API = "False";
         SCARF_NO_ANALYTICS = "True";
         DO_NOT_TRACK = "True";
         ANONYMIZED_TELEMETRY = "False";
-        WEBUI_AUTH = "False";
-
-        # 5. STT (Whisper) Hardware Acceleration
-        WHISPER_MODEL = "base.en";
-        WHISPER_LANGUAGE = "en";
-        WHISPER_MULTILINGUAL = "False";
-        
-        # MOVE TO NVIDIA GPU
-        WHISPER_DEVICE = "cuda";
-        
-        # CRITICAL: Compress to fit in the remaining 500MB of VRAM
-        WHISPER_COMPUTE_TYPE = "int8"; 
       };
     };
   };
