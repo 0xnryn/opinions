8b2e95250397930bcd4a8d169cee584c8d777260
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-06-14 13:11:05 +0530

    update

--- modules/nixosConfig/ollama.nix
+++ modules/nixosConfig/ollama.nix
@@ -8,13 +8,22 @@
     };
   };
 
-  flake.nixosModules.ollama_vulkan = { pkgs, ... }:
+  { ... }:
   {
-    services.ollama = {
-      enable = true;
-      package = pkgs.ollama-vulkan; 
+    flake.nixosModules.ollama_amd = { pkgs, ... }:
+    {
+      services.ollama = {
+        enable = true;
+        package = pkgs.ollama-vulkan; 
+        
+        environmentVariables = {
+          # Force Vulkan to ONLY see the AMD iGPU (GPU0)
+          # This completely hides the 4GB RTX 3050 (GPU1) from Ollama
+          GGML_VK_VISIBLE_DEVICES = "0"; 
+        };
+      };
     };
-  };
+  }
   
   flake.nixosModules.ollama = { pkgs, ... }:
   {
