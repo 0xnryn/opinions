e7cd38bf51395f5392a1db2d2162e30e3f1cf585
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-06-14 22:01:27 +0530

    udpate

--- modules/nixosConfig/openwebui.nix
+++ modules/nixosConfig/openwebui.nix
@@ -1,8 +1,9 @@
 { ... }:
 {
-  flake.nixosModules.openwebui = { ... }: {
+  flake.nixosModules.sudha-openwebui = { ... }: {
     services.open-webui = {
       enable = true;
+      host = "0.0.0.0";
       port = 8080;
       environment = {
         SCARF_NO_ANALYTICS = "True";
